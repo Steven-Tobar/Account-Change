c71ce64dc12ae36c5cfaa37393e33de09b96d909
Steven-Tobar <steven.tobar@gmail.com>
2018-04-16 15:41:51 -0400

    script for checking samaccount name

---
+++ Get potential users.ps1
@@ -0,0 +1,9 @@
+﻿$user = "steven.tobar"
+
+$firstname,$lastname =$user.Split(".")
+
+$fullname = $firstname + " " + $lastname
+
+$verifyuser = Get-ADUser -filter {name -eq $fullname} |select -expandproperty samaccountname 
+
+echo "The user's username is actually: $verifyuser"

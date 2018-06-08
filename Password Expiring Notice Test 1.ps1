3f5bd5473731de46f0a99fe00cbf008abec8bcd4
Tobar <Steven.Tobar@newyork.hbpub.net>
2018-06-08 11:32:09 -0400

    Testing for password expiring dates

---
+++ Password Expiring Notice Test 1.ps1
@@ -0,0 +1,28 @@
+$user  = get-aduser steven.tobar -properties *
+
+
+$x = $user.PasswordLastSet
+
+if ($x -eq $x.AddDays(+31))
+{
+   Write-host "Password expiring in 2 weeks" -ForegroundColor Yellow 
+} 
+
+elseif ($x -eq $x.AddDays(+38))
+{
+   Write-host "Password expiring in 1 week" -ForegroundColor Cyan
+}
+
+elseif($x -eq $x.AddDays(+42))
+{
+   Write-Host "Password expiring in 3 days" -ForegroundColor Magenta
+}
+
+else
+{
+   write-host "You good." -ForegroundColor green
+}
+
+
+
+

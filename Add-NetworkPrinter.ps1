0756bf293decbb368527b16b6b4ea7bbd2372158
Tobar <Steven.Tobar@newyork.hbpub.net>
2018-07-24 11:22:51 -0400

    Testing adding network printers

---
+++ Add-NetworkPrinter.ps1
@@ -0,0 +1,13 @@
+﻿$CurrentPrinters = Get-Printer
+$NetworkPrinter = "printer path here"
+if ($CurrentPrinters.name -eq $UniflowPrinter)
+{
+    Write-host "The network printer is already installed." -ForegroundColor Yellow
+
+}
+else
+{
+    Add-Printer -ConnectionName $NetworkPrinter
+    Write-Host "The printer has been added." -ForegroundColor Yellow
+
+}
